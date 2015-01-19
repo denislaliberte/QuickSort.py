cbfb210b3e1c69339a9e1b4eb24d30ba89722ade
Denis Laliberté <denis@tp1.ca>
2015-01-19 13:02:08 -0500

    refactor millde function to return the value

--- QuickSort.py
+++ QuickSort.py
@@ -1,13 +1,15 @@
 
+def middle_value(input):
+  index = round(len(input) /2) -1
+  return input[index]
+
 def sort(input):
   if len(input) <= 1:
     return input
   else:
-    middle_index = middle(input)
-    lower = [item for item in input if item < input[middle_index]]
-    pivot = [item for item in input if item == input[middle_index]]
-    upper = [item for item in input if item > input[middle_index]]
+    middle = middle_value(input)
+    lower = [item for item in input if item <  middle]
+    pivot = [item for item in input if item == middle]
+    upper = [item for item in input if item >  middle]
     return lower + pivot + upper
 
-def middle(input):
-  return round(len(input) /2) -1
--- QuickSort_test.py
+++ QuickSort_test.py
@@ -11,4 +11,4 @@ class sort_test(unittest.TestCase):
     self.assertEqual(sort([3,2,1]),[1,2,3])
     self.assertEqual(sort([-1,2,3]),[-1,2,3])
   def test_middle_index(self):
-    self.assertEqual(middle([1,2,3]),1)
+    self.assertEqual(middle_value([1,2,3]),2)
